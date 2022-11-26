2f3c6d8aee6556d6c523c50a3e2bf1c2d93c9cfc
amr ismayl <105287289+amrIsmayl@users.noreply.github.com>
2022-11-26 18:07:18 +0200

    refactor all requires

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const port = process.env.PORT || 4000 // badeel lel port 3000
 
 var morgan = require('morgan') // module morgan bnstfed menh fe el data bta3t el request 
 const AppError = require('./src/utilts/AppError')
-const globalMiddlewareErr = require('./src/utilts/globalMiddlewareErr')
+const globalMiddlewareErr = require('./src/utilts/globalMiddlewareErr');
+const { allRequires } = require('./src/utilts');
 // bygeb kol el informaton el 5asa bel request men el type and size and time 
 
 // middleware
@@ -24,19 +25,10 @@ if (process.env.MODE_ENV === 'development') {
     app.use(morgan('dev')) // keda bytm useing el module
 };
 
-app.use('/categories', require('./src/components/category/category.api'));
-app.use('/subcategories', require('./src/components/subCategory/subCategory.api'));
-app.use('/brands', require('./src/components/brand/brand.api'));
-app.use('/product', require('./src/components/product/product.api'));
-app.use('/user', require('./src/components/user/user.api'));
-app.use('/review', require('./src/components/review/review.api'));
-app.use('/wishlist', require('./src/components/wishlist/wishlist.api'));
-app.use('/address', require('./src/components/address/address.api'));
-
-
+allRequires(app); // to all url require
 
 // global error handling in URL
-app.use('*', (req, res, next) => {
+app.all('*', (req, res, next) => {
     next(
         new AppError(`can't find this route: ${req.originalUrl} on server`, 404)
     )
---
+++ src/utilts/index.js
@@ -0,0 +1,11 @@
+
+exports.allRequires = (app) => {
+    app.use('/categories', require('../components/category/category.api'));
+    app.use('/subcategories', require('../components/subCategory/subCategory.api'));
+    app.use('/brands', require('../components/brand/brand.api'));
+    app.use('/product', require('../components/product/product.api'));
+    app.use('/user', require('../components/user/user.api'));
+    app.use('/review', require('../components/review/review.api'));
+    app.use('/wishlist', require('../components/wishlist/wishlist.api'));
+    app.use('/address', require('../components/address/address.api'));
+}
